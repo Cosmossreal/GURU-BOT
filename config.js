8e2d4c91844585fb77f284c6b91dd0fb642f686d
Cosmossreal <134201586+Cosmossreal@users.noreply.github.com>
2024-01-16 19:37:47 +0100

    Update config.js

--- config.js
+++ config.js
@@ -7,15 +7,15 @@ import axios from 'axios'
 
 
 global.owner = [
-  ['917605902011', 'GURU', true],
-  ['6281255369012', 'Ash', true], 
+  ['48732078915', '@𝘖𝘔𝘈𝘙 !', true],
+  ['48732078915', '𝘖𝘔𝘈𝘙 !', true], 
   [''] 
 ] //Number of owners
 
 //global.pairingNumber = "" //put your bot number here
 
-global.mods = ['917605902011','32460248586'] 
-global.prems = ['917605902011', '32460248586', '919398758484']
+global.mods = ['48732078915','48732078915'] 
+global.prems = ['48732078915', '48732078915', '48732078915']
 global.allowed = ['917605902011', '32460248586', '19152999993']
 global.keysZens = ['c2459db922', '37CC845916', '6fb0eff124']
 global.keysxxx = keysZens[Math.floor(keysZens.length * Math.random())]
